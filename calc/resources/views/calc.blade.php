8dd95d3c2d6f02bc49725a32c30f828652beba51
Ilya <ilyamegaboy228@gamil.com>
2020-03-02 14:18:13 +0500

    *Fronted part fix

--- calc/resources/views/calc.blade.php
+++ calc/resources/views/calc.blade.php
@@ -2,12 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css">
-    <script
-            src="https://code.jquery.com/jquery-3.1.1.min.js"
-            integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
-            crossorigin="anonymous"></script>
-    <script src="semantic/dist/semantic.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="front.css" />
     <title>HTML калькулятор</title>
 </head>
 <body>
