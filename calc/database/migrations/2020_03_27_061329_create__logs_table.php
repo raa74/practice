7342a8cc7a5b751ca6c85e3a57f4d1385ea98920
RayneKris <rayne.kris902@gmail.com>
2020-03-27 18:28:01 +0500

    Создание модели "HistoryCalc"
    Изменение таблицы "Log", удалены дефолтные миграции, создана модель HistoryCalc ?и связана с таблицей "Logs"?

--- calc/database/migrations/2020_03_27_061329_create__logs_table.php
+++ calc/database/migrations/2020_03_27_061329_create__logs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateLogTable extends Migration
+class CreateLogsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateLogTable extends Migration
      */
     public function up()
     {
-        Schema::create('Log', function (Blueprint $table) {
+        Schema::create('Logs', function (Blueprint $table) {
             $table->increments('id');
             $table->date('Date');
             $table->string('Mathstring', 150);
@@ -28,6 +28,6 @@ class CreateLogTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('_log');
+        Schema::dropIfExists('Logs');
     }
 }
