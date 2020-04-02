2bb6a14243ed02316c5d256c694fd2276f9b01c2
RayneKris <rayne.kris902@gmail.com>
2020-04-02 17:46:39 +0500

    Работа над методом записи в таблицу

--- calc/app/ServiceClasses/Recording.php
+++ calc/app/ServiceClasses/Recording.php
@@ -1,16 +1,15 @@
 <?php
 
-
 namespace App\ServiceClasses;
-
 
 use App\HistoryCalc;
 use App\Http\Controllers\MyMathController;
+
 class Recording
 {
-    //public static function Record() {
-    //$log = new HistoryCalc();
-    //$log->Mathstring = '';
-    //$log->save();
-//}
+    public static function Record(MyMathController $result) {
+    $log = new HistoryCalc();
+    $log->Mathstring = $result;
+    $log->save();
+}
 }
